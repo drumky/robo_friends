f247c6005e62eaeb8414a62dd4a4effa92e5e4e7
drumky <axeljcob@gmail.com>
2020-04-26 13:54:34 +0800

    rendering event change on the SearchBox

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "tachyons";
 import { robots } from "./robots";
-import CardList from "./CardList";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <CardList robots={robots} />
+    <App />,
   </React.StrictMode>,
   document.getElementById("root")
 );
